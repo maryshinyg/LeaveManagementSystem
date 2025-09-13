0458e6d1348b20c8e8f1f2162b880a737111d34e
marys <maryshiny16@gmail.com>
2025-09-13 12:37:52 +0530

    Completed Leave Type module CRUD operations

---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        [RegularExpression(@"^[a-zA-Z]{1,50}$", ErrorMessage = "Name must be 1 to 50 letters only.")]
+        public string Name { get; set; }
+        public int NumberOfDays { get; set; }
+    }
+}
